b6641c30b4cec9c9cf02582fdb6dd63bac126d37
AntonAsmanov <antonasmanov@yandex.ru>
2022-09-15 22:54:41 +0500

    add JUnitDemoTest

---
+++ src/test/java/com/demoqa/JUniteDemoTest.java
@@ -0,0 +1,41 @@
+package com.demoqa;
+
+import org.junit.jupiter.api.*;
+
+public class JUniteDemoTest {
+
+    @BeforeAll
+    static void setUp(){
+        System.out.println("### @BeforeAll");
+    }
+
+    @AfterAll
+    static void tearUp(){
+        System.out.println("### @AfterAll");
+    }
+
+    @BeforeEach
+    void beforeEachTest(){
+        // open("ya.test");
+        System.out.println("###     @beforeEachTest");
+    }
+
+    @AfterEach
+    void afterEachTest(){
+        System.out.println("###     @afterEachTest");
+    }
+
+
+    @Test
+    void simpeTestFirst(){
+        System.out.println("###         @Test simpeTestFirst");
+        Assertions.assertTrue(3>2);
+    }
+
+    @Test
+    void simpeTestSecond(){
+        System.out.println("###         @Test simpeTestSecond");
+        Assertions.assertTrue(3>2);
+    }
+
+}
